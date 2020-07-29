eb2ed0d20ed7b5bb122371de08af5714cdf84be7
Dohyun Kim <unhyop@gmail.com>
2020-07-29 16:24:10 +0900

    Update Mixed.java

--- Mixed.java
+++ Mixed.java
@@ -1,22 +1,27 @@
 package com.company.interitance;
 
 public class Mixed {
-	private int counter;
-	private int sum;
+  private int counter;
+  private int sum;
 	
   private Mixed() {
     super();
   }
 
-	public final int apiForClients() {
-		int subclass = impl.toBeImplementBySubclass();
+  // 외부로 노출한 API 메서드
+  public final int apiForClients() {
+    int subclass = impl.toBeImplementBySubclass();
 
-		sum += subclass;
-		int result = sum / counter;
+    // 필요한 로직 구현
+    sum += subclass;
+    int result = sum / counter;
 
-		return result;
-	}
+    return result;
+  }
 
-	protected abstract int toBeImplementBySubclass();
-	protected final void toBeCalledBySubclass() { counter++; }
+  // 하위 클래스에서 구현해야 한다.
+  protected abstract int toBeImplementBySubclass();
+
+  // 하위 클래스는 이 메서드를 호출할 수 있다.
+  protected final void toBeCalledBySubclass() { counter++; }
 }
