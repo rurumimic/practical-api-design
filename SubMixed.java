870f985ffbc5f88aa01b08f157d12d61d349e9b3
Dohyun Kim <unhyop@gmail.com>
2020-07-29 16:33:19 +0900

    Update SubMixed.java

--- SubMixed.java
+++ SubMixed.java
@@ -1,4 +1,4 @@
-package com.company.interitance;
+//package com.company.interitance;
 
 public class SubMixed extends Mixed {
   private SubMixed() {
@@ -6,9 +6,9 @@ public class SubMixed extends Mixed {
   }
 
   // 하위 클래스에서 구현해야 한다.
-	@Override
-	protected int toBeImplementBySubclass() {
-		toBeCalledBySubclass(); // 상위 클래스의 메서드를 호출할 수 있다.
-		return 7;
+  @Override
+  protected int toBeImplementBySubclass() {
+    toBeCalledBySubclass(); // 상위 클래스의 메서드를 호출할 수 있다.
+    return 7;
   }
 }
